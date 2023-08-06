f653b3e214b416de3325e6ba6e63a7790088155f
mehmet ozdemir <m.ozd23@gmail.com>
2023-08-06 20:21:18 +0200

    feat: add review to service

---
+++ database/migrations/2023_08_05_222024_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reviewer_id');
+            $table->unsignedBigInteger('reviewee_id');
+            $table->text('comment');
+            $table->integer('rating');
+            $table->string('type');
+            $table->timestamps();
+
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->foreign('reviewer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('reviewee_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,115 +2,105 @@
 
 namespace Database\Seeders;
 
+use App\Models\Review;
 use App\Models\Service;
+use App\Models\ServiceRequest;
 use App\Models\Skill;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // Create users with the password 'blabla'
-        $users = [
-            [
-                'name' => 'John Doe',
-                'email' => 'john.doe@example.com',
-                'password' => bcrypt('blabla'),
-            ],
-            [
-                'name' => 'Jane Smith',
-                'email' => 'jane.smith@example.com',
-                'password' => bcrypt('blabla'),
-            ],
-            [
-                'name' => 'Michael Johnson',
-                'email' => 'michael.johnson@example.com',
-                'password' => bcrypt('blabla'),
-            ],
-            [
-                'name' => 'Emily Williams',
-                'email' => 'emily.williams@example.com',
-                'password' => bcrypt('blabla'),
-            ],
-        ];
+        // Create users
+        $user1 = User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        // Store users and get their IDs
-        $userIds = [];
-        foreach ($users as $user) {
-            $userIds[] = DB::table('users')->insertGetId($user);
-        }
+        $user2 = User::create([
+            'name' => 'Jane Smith',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        // Store skills
-        $skills = [
-            'PHP',
-            'JavaScript',
-            'Python',
-            'Java',
-        ];
+        // Create skills
+        $skill1 = Skill::create([
+            'name' => 'Web Development',
+        ]);
 
-        foreach ($skills as $skillName) {
-            Skill::create(['name' => $skillName]);
-        }
+        $skill2 = Skill::create([
+            'name' => 'Database Management',
+        ]);
 
-        // Associate 4 different skills to each user
-        foreach ($userIds as $userId) {
-            $user = User::find($userId);
-            $skills = Skill::inRandomOrder()->take(4)->get();
-            $user->skills()->attach($skills);
-        }
+        // Create services offered
+        $service1 = Service::create([
+            'user_id' => $user1->id,
+            'skill_id' => $skill1->id,
+            'title' => 'Custom Website Development',
+            'description' => 'I will build a custom website for your business using the latest technologies.',
+        ]);
 
-        // Create 3 different services for each skill of each user.
-        foreach ($userIds as $userId) {
-            $user = User::find($userId);
-            foreach ($user->skills as $skill) {
-                $existingTitles = Service::where('user_id', '!=', $userId)->pluck('title')->toArray();
-                $servicesData = [];
-                for ($i = 1; $i <= 3; $i++) {
-                    $title = $this->getServiceTitle($skill->name, $i, $existingTitles);
-                    $description = $this->getServiceDescription($skill->name, $i);
-                    $existingTitles[] = $title;
-                    $servicesData[] = [
-                        'user_id' => $user->id,
-                        'skill_id' => $skill->id,
-                        'title' => $title,
-                        'description' => $description,
-                    ];
-                }
-                Service::insert($servicesData);
-            }
-        }
-    }
+        $service2 = Service::create([
+            'user_id' => $user2->id,
+            'skill_id' => $skill2->id,
+            'title' => 'Database Optimization',
+            'description' => 'I will optimize your database queries for better performance.',
+        ]);
 
-    private function getServiceTitle(string $skillName, int $index, array $existingTitles): string
-    {
-        $titles = [
-            'Custom E-commerce Website Development',
-            'Full-stack Web Application Development',
-            'API Integration and Backend Development',
-            'Mobile App Development',
-        ];
-        $title = $titles[$index - 1] . ' (' . $skillName . ')';
-        $suffix = 2;
-        while (in_array($title, $existingTitles)) {
-            $title = $titles[$index - 1] . ' (' . $skillName . ' - ' . $suffix . ')';
-            $suffix++;
-        }
-        return $title;
-    }
+        // Create service requests
+        $serviceRequest1 = ServiceRequest::create([
+            'user_id' => $user2->id,
+            'service_id' => $service1->id,
+            'notes' => 'I need a custom website for my small business.',
+        ]);
 
-    private function getServiceDescription(string $skillName, int $index): string
-    {
-        $descriptions = [
-            'I will create a custom e-commerce website tailored to your business needs using the latest technologies.',
-            'Get a fully functional and user-friendly web application that meets your specific requirements.',
-            'Integrate APIs into your system and build a robust backend to support your application.',
-            'Hire me to develop a mobile app for Android and iOS platforms.',
-        ];
-        return $descriptions[$index - 1];
+        $serviceRequest2 = ServiceRequest::create([
+            'user_id' => $user1->id,
+            'service_id' => $service2->id,
+            'notes' => 'I need help optimizing my database queries.',
+        ]);
+
+        // Create reviews
+        $review1 = Review::create([
+            'service_id' => $service1->id,
+            'reviewer_id' => $user2->id,
+            'reviewee_id' => $user1->id,
+            'comment' => 'John did an excellent job building my website. Highly recommended!',
+            'rating' => 5,
+            'type' => 'received',
+        ]);
+
+        $review2 = Review::create([
+            'service_id' => $service2->id,
+            'reviewer_id' => $user1->id,
+            'reviewee_id' => $user2->id,
+            'comment' => 'Jane helped me optimize my database queries, and the performance improvements were amazing!',
+            'rating' => 4,
+            'type' => 'received',
+        ]);
+
+        $review3 = Review::create([
+            'service_id' => $service1->id,
+            'reviewer_id' => $user1->id,
+            'reviewee_id' => $user2->id,
+            'comment' => 'Jane requested my web development service and was a pleasure to work with.',
+            'rating' => 4,
+            'type' => 'left',
+        ]);
+
+        $review4 = Review::create([
+            'service_id' => $service2->id,
+            'reviewer_id' => $user2->id,
+            'reviewee_id' => $user1->id,
+            'comment' => 'John provided valuable insights into database optimization.',
+            'rating' => 5,
+            'type' => 'left',
+        ]);
     }
 }
