350a38cdc3c0a4b8231af8d5179e79914e9dcd90
mehmet ozdemir <m.ozd23@gmail.com>
2023-08-17 01:55:15 +0200

    fix: change bg color of main layout and app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,12 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    {{-- <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png" /> --}}
-    <title>Soft UI Dashboard Tailwind</title>
+    <title>Skillmate - Share your skills</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-gray-50 dark:bg-gray-800">
+<body class="bg-white">
     @yield('app-content')
 </body>
 
