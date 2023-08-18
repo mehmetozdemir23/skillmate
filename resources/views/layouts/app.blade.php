056f7999a3d4adf146ad61bb1dca8eae05256223
mehmet ozdemir <m.ozd23@gmail.com>
2023-08-18 23:30:18 +0200

    Add favicon and change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Skillmate - Share your skills</title>
-
+    <title>Skillmate - Your IT Services Hub</title>
+    <link rel="icon" href="{{ asset('assets/icons/skillmate-logo.png') }}" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
